ecf496f8dea9f2e4d628d70f2e9d078168ff6bc1
saucesar <sau.cesarlima2013@gmail.com>
2021-05-09 23:40:32 -0300

    ajuste de middleware das rotas.

--- routes/web.php
+++ routes/web.php
@@ -24,57 +24,58 @@ Route::get('/', function () {
         return redirect()->route('home');
     }
 });
-
-Route::get('admin-area', 'AdminAreaController@index')->name('admin.area');
-Route::get('admin-area/machines', 'AdminAreaController@machines')->name('admin.area.machines');
-Route::get('admin-area/users', 'AdminAreaController@users')->name('admin.area.users');
-
-Route::resource('machines', 'MaquinasController')->except('index')->middleware('auth');
-Route::resource('images', 'ImagesController')->middleware('auth');
-
-Route::get('containers-instace', 'ContainersController@index')->name('containers.index');
-Route::post('containers-instace', 'ContainersController@configureContainer')->name('containers.configure');
-Route::get('terminal-tab/{docker_id}', 'ContainersController@terminalNewTab')->name('container.terminalTab');
-Route::resource('containers', 'ContainersController')->except(['create', 'index']);
-Route::get('containers/play-stop/{containerId}', 'ContainersController@playStop')->name('containers.playStop');
-Route::post('containers/{docker_id}/delete', 'ContainersController@deleteContainer')->name('container.delete');
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
-
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('table-list', function () {
-        return view('pages.table_list');
-    })->name('table');
 
-    Route::get('user-machines', 'MaquinasController@machines')->name('user.machines');
-});
+    Route::get('admin-area', 'AdminAreaController@index')->name('admin.area');
+    Route::get('admin-area/machines', 'AdminAreaController@machines')->name('admin.area.machines');
+    Route::get('admin-area/users', 'AdminAreaController@users')->name('admin.area.users');
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::resource('user', 'UserController', ['except' => ['show']]);
-    Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
-    Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
-    Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
-});
+    Route::resource('machines', 'MaquinasController')->except('index')->middleware('auth');
+    Route::resource('images', 'ImagesController')->middleware('auth');
 
-Route::resource('services', 'ServiceController');
-Route::get('docker-swarm', 'DockerSwarmController@index')->name('docker-swarm.index');
-Route::post('docker-swarm', 'DockerSwarmController@swarmInit')->name('docker-swarm.init');
-Route::post('docker-swarm/leave', 'DockerSwarmController@swarmLeave')->name('docker-swarm.leave');
+    Route::get('containers-instace', 'ContainersController@index')->name('containers.index');
+    Route::post('containers-instace', 'ContainersController@configureContainer')->name('containers.configure');
+    Route::get('terminal-tab/{docker_id}', 'ContainersController@terminalNewTab')->name('container.terminalTab');
+    Route::resource('containers', 'ContainersController')->except(['create', 'index']);
+    Route::get('containers/play-stop/{containerId}', 'ContainersController@playStop')->name('containers.playStop');
+    Route::post('containers/{docker_id}/delete', 'ContainersController@deleteContainer')->name('container.delete');
 
-Route::prefix('settings')->group(function(){
-    Route::get('/', 'SettingsController@index')->name('settings.index');
-    Route::put('/service-template/update', 'SettingsController@updateServiceTemplate')->name('service-template.update');
-    Route::put('/container-template/update', 'SettingsController@updateContainerTemplate')->name('container-template.update');
-    Route::put('/volume-driver-template/update', 'SettingsController@updateVolumeDriver')->name('volume-driver.update');
-    Route::get('/phpinfo', 'SettingsController@phpinfo')->name('settings.phpinfo');
-});
+    Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
-Route::resource('user-categories', 'UserCategoryController', ['except' => ['show', 'index', 'create', 'edit']]);
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('table-list', function () {
+            return view('pages.table_list');
+        })->name('table');
 
-Route::prefix('swarm-nodes')->group(function(){
-    Route::get('/', 'NodesController@index')->name('nodes.index');
-    Route::put('/{id}/{version}', 'NodesController@updateNode')->name('nodes.update');
-    Route::delete('/{id}', 'NodesController@destroyNode')->name('nodes.destroy');
+        Route::get('user-machines', 'MaquinasController@machines')->name('user.machines');
+    });
+
+        Route::resource('user', 'UserController', ['except' => ['show']]);
+        Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+        Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+        Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+
+    Route::resource('services', 'ServiceController');
+    Route::get('docker-swarm', 'DockerSwarmController@index')->name('docker-swarm.index');
+    Route::post('docker-swarm', 'DockerSwarmController@swarmInit')->name('docker-swarm.init');
+    Route::post('docker-swarm/leave', 'DockerSwarmController@swarmLeave')->name('docker-swarm.leave');
+
+    Route::prefix('settings')->group(function(){
+        Route::get('/', 'SettingsController@index')->name('settings.index');
+        Route::put('/service-template/update', 'SettingsController@updateServiceTemplate')->name('service-template.update');
+        Route::put('/container-template/update', 'SettingsController@updateContainerTemplate')->name('container-template.update');
+        Route::put('/volume-driver-template/update', 'SettingsController@updateVolumeDriver')->name('volume-driver.update');
+        Route::get('/phpinfo', 'SettingsController@phpinfo')->name('settings.phpinfo');
+    });
+
+    Route::resource('user-categories', 'UserCategoryController', ['except' => ['show', 'index', 'create', 'edit']]);
+
+    Route::prefix('swarm-nodes')->group(function(){
+        Route::get('/', 'NodesController@index')->name('nodes.index');
+        Route::put('/{id}/{version}', 'NodesController@updateNode')->name('nodes.update');
+        Route::delete('/{id}', 'NodesController@destroyNode')->name('nodes.destroy');
+    });
+
 });
