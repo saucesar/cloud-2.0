28184ca2261266454e975bc26f92a1c71b204770
saucesar <sau.cesarlima2013@gmail.com>
2021-06-11 10:54:22 -0300

    Ajustes para conexão com o websocket

--- resources/views/pages/my-containers/my_containers_terminal_tab.blade.php
+++ resources/views/pages/my-containers/my_containers_terminal_tab.blade.php
@@ -23,20 +23,23 @@
 
 @push('js')
 <script>
-var socketParams = <?= $socketParams; ?>;
-var host = socketParams['dockerHost'];
-var containerId = socketParams['container_id'];
-var endpoint = "/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1";
-
+const host = "<?= $dockerHost ?>";
+const containerId = "<?= $containerId ?>";
+const endpoint = "/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1";
 const url = host+'/containers/'+containerId+endpoint;
 
 const webSocket = new WebSocket(url);
-webSocket.onopen = function (e) { webSocket.send("\n"); }
+webSocket.onopen = function (e) {
+    //alert("Connected: "+(webSocket.readyState == 1));
+    webSocket.send("\n");
+}
+//webSocket.onmessage = function (e) {term.write(e.data);}
+webSocket.onclose = function(e) {console.log(e); alert("Connected: "+(webSocket.readyState == 1));}
 
 const attachAddon = new AttachAddon.AttachAddon(webSocket);
 const fitAddon = new FitAddon.FitAddon();
 const webLinksAddon = new WebLinksAddon.WebLinksAddon();
-const term = new Terminal();
+const term = new Terminal({cursorBlink: true});
 
 term.loadAddon(attachAddon);
 term.loadAddon(fitAddon);
@@ -45,5 +48,6 @@ term.loadAddon(webLinksAddon);
 term.open(document.getElementById('terminal'));
 fitAddon.fit();
 term.reset();
+term.focus();
 </script>
 @endpush
